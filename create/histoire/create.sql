9c69be7dc6ea58479b8f913c86e8a5e75a33f6be
Nicolas Endredi <nendredi@scorpion.emi.u-bordeaux1.fr>
2013-02-18 22:42:24 +0100

    Creation de la premiere version des table_create du module histoire [A terminer]

---
+++ create/histoire/create.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS `Histoire`
+(
+	auteur varchar(40),
+	titre varchar(40),
+	date_creation date,
+	date_modification date,
+	age_requis int(11)
+);
---
+++ create/histoire/index.sql
@@ -0,0 +1,4 @@
+SOURCE ./create/histoire/evenement/create.sql
+SOURCE ./create/histoire/choix/create.sql
+SOURCE ./scenario/histoire/choix/create.sql
+SOURCE ./create/histoire/create.sql
