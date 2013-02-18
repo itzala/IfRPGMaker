74707d6586d844dbbad9a046290939689d6e5352
Itzala <boruezabal@gmail.com>
2013-02-18 18:31:19 +0100

    Ajout des fichiers pour reset (à compléter)

---
+++ reset/all_tables.sql
@@ -0,0 +1,3 @@
+SOURCE ./histoire/reset_tables.sql;
+SOURCE ./jeu/reset_tables.sql;
+SOURCE ./joueur/reset_tables.sql
---
+++ reset/jeu/reset_tables.sql
@@ -0,0 +1,8 @@
+# On désactive les contraintes de clés étrangères
+set foreign_key_checks 0;
+
+# On supprime toutes les tables
+
+
+# On résactive les contraintes de clés étrangères
+set foreign_key_checks 1;
