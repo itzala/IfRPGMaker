d12e8e1a65090c88c8555a4c38e98e6f1ecc77f0
Nicolas Endredi <endredi.nicolas@gmail.com>
2013-02-23 16:22:05 +0100

    reset de histoire créés

--- reset/histoire/all_contenu.sql
+++ reset/histoire/all_contenu.sql
@@ -3,6 +3,12 @@ SET foreign_key_checks = 0;
 
 # On vide toutes les tables
 
+TRUNCATE IF EXISTS `Description`;
+TRUNCATE IF EXISTS `Intro`;
+TRUNCATE IF EXISTS `Evenement`;
+TRUNCATE IF EXISTS `Histoire`;
+TRUNCATE IF EXISTS `Choix`;
+TRUNCATE IF EXISTS `Scenario`;
 
 # On résactive les contraintes de clés étrangères
 SET foreign_key_checks = 1;
--- reset/histoire/all_tables.sql
+++ reset/histoire/all_tables.sql
@@ -2,7 +2,12 @@
 SET foreign_key_checks = 0;
 
 # On supprime toutes les tables
+DROP TABLE IF EXISTS `Description`;
+DROP TABLE IF EXISTS `Intro`;
+DROP TABLE IF EXISTS `Evenement`;
+DROP TABLE IF EXISTS `Histoire`;
+DROP TABLE IF EXISTS `Choix`;
+DROP TABLE IF EXISTS `Scenario`;
 
-
-# On résactive les contraintes de clés étrangères
+# On réactive les contraintes de clés étrangères
 SET foreign_key_checks = 1;
