d8320015cecbc2c2da2375f5e87aebc17e4298ae
Itzala <boruezabal@gmail.com>
2013-02-28 23:00:15 +0100

    Nouvelle arborescence dans fixtures/

---
+++ fixtures/joueur/all.sql
@@ -0,0 +1,2 @@
+SOURCE ./fixtures/joueur/all_tables.sql
+SOURCE ./fixtures/joueur/all_contenu.sql
---
+++ fixtures/systeme_jeu/all_tables.sql
@@ -0,0 +1,3 @@
+SOURCE ./fixtures/systeme_jeu/caracteristique/all_tables.sql;
+SOURCE ./fixtures/systeme_jeu/race/all_tables.sql;
+SOURCE ./fixtures/systeme_jeu/equipement/all_tables.sql;
