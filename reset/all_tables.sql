20e25c261a5806cfaac3a9084f3e8dcdf73d4c3c
Nicolas Endredi <nendredi@scorpion.emi.u-bordeaux1.fr>
2013-03-27 20:35:42 +0100

    Ajout du reset/contrainte au reset/all_tables

--- reset/all_tables.sql
+++ reset/all_tables.sql
@@ -5,3 +5,4 @@ SOURCE ./reset/histoire/all_tables.sql
 #SOURCE ./reset/actions/all_tables.sql
 #SOURCE ./reset/dialogues/all_tables.sql
 #SOURCE ./reset/config/all_tables.sql
+SOURCE ./reset/contrainte/all_tables.sql
