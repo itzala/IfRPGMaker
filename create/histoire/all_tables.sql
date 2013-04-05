658de4cbbb9b8444acba2b6d9351a766ee8ac494
Nicolas Endredi <nendredi@emi.u-bordeaux1.fr>
2013-04-05 16:16:59 +0200

    Commit de la modification de Histoire

--- create/histoire/all_tables.sql
+++ create/histoire/all_tables.sql
@@ -3,7 +3,7 @@ SOURCE ./create/histoire/evenement/all_tables.sql
 
 CREATE TABLE IF NOT EXISTS `Histoire`
 (
-	auteur varchar(40),
+	auteur varchar(40) NOT NULL,
 	titre varchar(40),
 
 	date_creation date,
@@ -14,7 +14,8 @@ CREATE TABLE IF NOT EXISTS `Histoire`
 	contrainte int(11),
 
 	PRIMARY KEY (auteur, titre),
-	FOREIGN KEY (contrainte) REFERENCES ElementContrainte(id_element) ON DELETE CASCADE ON UPDATE CASCADE
+	FOREIGN KEY (contrainte) REFERENCES ElementContrainte(id_element) ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY (auteur) REFERENCES Joueur(pseudo) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE = INNODB;
 
 SOURCE ./create/histoire/choix/all_tables.sql
