44fbd5b763e2b2f1945f83b495aece3b2e4a9a08
Nicolas Endredi <nendredi@emi.u-bordeaux1.fr>
2013-04-05 16:15:58 +0200

    Suppression de la table Evenement car inutile

--- create/histoire/all_tables.sql
+++ create/histoire/all_tables.sql
@@ -9,13 +9,12 @@ CREATE TABLE IF NOT EXISTS `Histoire`
 	date_creation date,
 	date_modification date,
 	
-	description int(11) NOT NULL,
+	description int(11),
 
 	contrainte int(11),
 
 	PRIMARY KEY (auteur, titre),
-	FOREIGN KEY (contrainte) REFERENCES ElementContrainte(id_element) ON DELETE CASCADE ON UPDATE CASCADE,
-	FOREIGN KEY (description) REFERENCES Description(id) ON DELETE CASCADE ON UPDATE CASCADE
+	FOREIGN KEY (contrainte) REFERENCES ElementContrainte(id_element) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE = INNODB;
 
 SOURCE ./create/histoire/choix/all_tables.sql
--- create/histoire/evenement/all_tables.sql
+++ create/histoire/evenement/all_tables.sql
@@ -15,16 +15,3 @@ CREATE TABLE IF NOT EXISTS `Intro`
 
 	FOREIGN KEY (contrainte) REFERENCES ElementContrainte(id_element)
 ) ENGINE = INNODB;
-
-CREATE TABLE IF NOT EXISTS `Evenement`
-(
-	intro int(11) NOT NULL,
-	description int(11) NOT NULL,
-	contrainte int(11),
-
-	PRIMARY KEY (intro, description),
-	
-	FOREIGN KEY (intro) REFERENCES Intro(id) ON DELETE CASCADE ON UPDATE CASCADE,
-	FOREIGN KEY (description) REFERENCES Description(id) ON DELETE CASCADE ON UPDATE CASCADE,
-	FOREIGN KEY (contrainte) REFERENCES ElementContrainte(id_element)
-) ENGINE = INNODB;
