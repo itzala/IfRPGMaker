eec5660127fd1d3b0ca6868a4facf2fdc21229ed
aquaj <aquajvalin@gmail.com>
2013-04-05 21:31:09 +0200

    Modification de ActiDico en ActiKey suite a l'ajout de Keywords du a des problèmes de relations.

--- create/actions/all_tables.sql
+++ create/actions/all_tables.sql
@@ -1,11 +1,11 @@
-CREATE TABLE IF NOT EXISTS `ActiDico`
+CREATE TABLE IF NOT EXISTS `ActiKey`
 (
 	keyword varchar(40) NOT NULL,
 	action varchar(40) NOT NULL,
 
 	PRIMARY KEY (keyword, actiontext);
 	
-	FOREIGN KEY keyword REFERENCES Dictionnaire(keyword)ON DELETE CASCADE ON UPDATE CASCADE
+	FOREIGN KEY keyword REFERENCES Keywords(keyword)ON DELETE CASCADE ON UPDATE CASCADE
 	FOREIGN KEY action REFERENCES Actions(action) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE = INNODB;
 
@@ -15,12 +15,19 @@ CREATE TABLE IF NOT EXISTS `Actions`
 
 ) ENGINE = INNODB;
 
+CREATE TABLE IF NOT EXISTS `Keywords`
+(
+	keyword varchar(40) PRIMARY KEY,
+
+) ENGINE = INNODB;
+
 CREATE TABLE IF NOT EXISTS `Dictionnaire`
 (
 	keyword varchar(40) NOT NULL,
 	synonym varchar(40) NOT NULL,
 
-	PRIMARY KEY (keyword, synonym);
+	FOREIGN KEY keyword REFERENCES Keywords(keyword)ON DELETE CASCADE ON UPDATE CASCADE
+	PRIMARY KEY (synonym);
 ) ENGINE = INNODB;
 
 SOURCE ./all_contenu.sql;
