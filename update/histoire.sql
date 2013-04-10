7c0fb6c3d2cebd29722198011a52f8924e079085
Nicolas Endredi <nendredi@emi.u-bordeaux1.fr>
2013-04-10 11:33:54 +0200

    correction des backquotes dans histoire

--- update/histoire.sql
+++ update/histoire.sql
@@ -1,13 +1,13 @@
-UPDATE `Intro` SET contenu='%PARAM_CONTENU%' WHERE id=%PARAM_ID%;
-UPDATE `Description` SET contenu='%PARAM_CONTENU%' WHERE id=%PARAM_ID%;
+UPDATE Intro SET contenu='%PARAM_CONTENU%' WHERE id=%PARAM_ID%;
+UPDATE Description SET contenu='%PARAM_CONTENU%' WHERE id=%PARAM_ID%;
 
-UPDATE 'Evenement' SET intro=%PARAM_INTRO%, description=%PARAM_DESCRIPTION% WHERE intro=%PARAM_INTRO% AND description=%PARAM_DESCRIPTION%;
-
-
-UPDATE `Choix` SET intro=%PARAM_INTRO%, description=%PARAM_DESCRIPTION%, parent=%PARAM_PARENT% WHERE id=%PARAM_ID%;
+UPDATE Evenement SET intro=%PARAM_INTRO%, description=%PARAM_DESCRIPTION% WHERE intro=%PARAM_INTRO% AND description=%PARAM_DESCRIPTION%;
 
 
-UPDATE `Scenario` SET auteur=%PARAM_AUTEUR%, titre=%PARAM_TITRE%, debut=%PARAM_DEBUT% WHERE auteur=%PARAM_AUTEUR% AND titre=%PARAM_TITRE% AND debut=%PARAM_DEBUT%;
+UPDATE Choix SET intro=%PARAM_INTRO%, description=%PARAM_DESCRIPTION%, parent=%PARAM_PARENT% WHERE id=%PARAM_ID%;
 
 
-UPDATE `Histoire` SET auteur=%PARAM_AUTEUR%, titre=%PARAM_TITRE% WHERE auteur=%PARAM_AUTEUR% AND titre=%PARAM_TITRE%;
+UPDATE Scenario SET auteur=%PARAM_AUTEUR%, titre=%PARAM_TITRE%, debut=%PARAM_DEBUT% WHERE auteur=%PARAM_AUTEUR% AND titre=%PARAM_TITRE% AND debut=%PARAM_DEBUT%;
+
+
+UPDATE Histoire SET auteur=%PARAM_AUTEUR%, titre=%PARAM_TITRE% WHERE auteur=%PARAM_AUTEUR% AND titre=%PARAM_TITRE%;
