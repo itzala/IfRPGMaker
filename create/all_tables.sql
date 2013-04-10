19dd49d13549cda64766e7e1f87f8fc3c0c324a6
Nicolas Endredi <nendredi@scorpion.emi.u-bordeaux1.fr>
2013-04-10 20:33:05 +0200

    début de l'implémentation des update pour le module Histoire

--- create/all_tables.sql
+++ create/all_tables.sql
@@ -5,8 +5,8 @@ CREATE TABLE IF NOT EXISTS `ElementContrainte`
 ) ENGINE = INNODB;
 
 SOURCE ./create/joueur/all_tables.sql;
-SOURCE ./create/systeme_jeu/all_tables.sql;
 SOURCE ./create/histoire/all_tables.sql;
+SOURCE ./create/systeme_jeu/all_tables.sql;
 SOURCE ./create/personnage/all_tables.sql;
 SOURCE ./create/actions/all_tables.sql;
 SOURCE ./create/dialogues/all_tables.sql;
--- update/histoire.sql
+++ update/histoire.sql
@@ -1,5 +1,5 @@
-UPDATE Intro SET contenu='%PARAM_CONTENU%' WHERE id=%PARAM_ID%;
-UPDATE Description SET contenu='%PARAM_CONTENU%' WHERE id=%PARAM_ID%;
+UPDATE Intro SET contenu="%PARAM_CONTENU%" WHERE id=%PARAM_ID%;
+UPDATE Description SET contenu="%PARAM_CONTENU%" WHERE id=%PARAM_ID%;
 
 UPDATE Evenement SET intro=%PARAM_INTRO%, description=%PARAM_DESCRIPTION% WHERE intro=%PARAM_INTRO% AND description=%PARAM_DESCRIPTION%;
 
@@ -7,7 +7,7 @@ UPDATE Evenement SET intro=%PARAM_INTRO%, description=%PARAM_DESCRIPTION% WHERE 
 UPDATE Choix SET intro=%PARAM_INTRO%, description=%PARAM_DESCRIPTION%, parent=%PARAM_PARENT% WHERE id=%PARAM_ID%;
 
 
-UPDATE Scenario SET auteur=%PARAM_AUTEUR%, titre=%PARAM_TITRE%, debut=%PARAM_DEBUT% WHERE auteur=%PARAM_AUTEUR% AND titre=%PARAM_TITRE% AND debut=%PARAM_DEBUT%;
+UPDATE Scenario SET auteur="%PARAM_AUTEUR%", titre="%PARAM_TITRE%", debut=%PARAM_DEBUT% WHERE auteur="%PARAM_AUTEUR%" AND titre="%PARAM_TITRE%" AND debut=%PARAM_DEBUT%;
 
 
-UPDATE Histoire SET auteur=%PARAM_AUTEUR%, titre=%PARAM_TITRE% WHERE auteur=%PARAM_AUTEUR% AND titre=%PARAM_TITRE%;
+UPDATE Histoire SET auteur="%PARAM_AUTEUR%", titre="%PARAM_TITRE%" WHERE auteur="%PARAM_AUTEUR%" AND titre="%PARAM_TITRE%";
